e97b026f5ff8fcf6d6df2dffd6e59cdf7da11a44
tarcisiocorte <tarcisiocorte@gmail.com>
2018-05-30 19:45:10 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/user/about">About</Link>
+        </li>
+        <li>
+          <Link to="/post/topics">Topics</Link>
+        </li>
+      </ul>
 
-class App extends Component {
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/user/:id" component={User} />
+      <Route path="/post/:id" component={Post} />
+    </div>
+  </Router>
+);
+class User extends Component {
   render() {
+    console.log(this.props.match);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>USER ID:{this.props.match.params.id}</h2>
       </div>
     );
   }
 }
+const Post = ({ match }) => (
+  <div>
+    <h2>POST ID:{match.params.id}</h2>
+  </div>
+);
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
 
-export default App;
+export default BasicExample;
